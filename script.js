7b2720cbb8c5fb87dccd8838bc878d87fabcebe7
Egor Gulid <egor-gulid@yandex.ru>
2022-03-25 21:27:32 +0300

    выполнил усложненное задание

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-
+let num = 266219;
+let array = ("" + num).split("").map(Number);
+console.log(array);
+let res = array.reduce((acc, rec) => acc * rec);
+console.log(res);
+let pow = 1296 ** 3;
+console.log(pow);
+let c = pow.toString();
+console.log(c[0]);
+console.log(c[1]);
